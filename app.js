af353da9c44c1ff9ab5a2f574cd4cce7cffb5bb4
SennayT <wsennay@gmail.com>
2020-10-25 20:51:58 +0300

    Added delete ability

--- app.js
+++ app.js
@@ -5,15 +5,25 @@ function renderCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "X";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  //Deleting data
+  cross.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("Cafes").doc(id).delete();
+  });
 }
 
 db.collection("Cafes")
@@ -36,4 +46,7 @@ form.addEventListener("submit", (e) => {
     name: form.name.value,
     city: form.city.value,
   });
+
+  form.name.value = "";
+  form.city.value = "";
 });
